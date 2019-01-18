11d7c4773e1daf3596cce32b33057a40f059b100
Thakee Chowdhury <thakeechow@gmail.com>
2019-01-17 19:08:43 -0500

    Update helps for commands and add basic logic for testing

--- passin/main.py
+++ passin/main.py
@@ -4,20 +4,28 @@ from .functions import password, gen_secretkey
 
 @click.group()
 def main():
+    """A command line tool for generating and managing passwords
+    at the exact same time."""
     pass
 
 
-@main.command()
-def setup():
+@main.command('setup', short_help='setup secret key')
+@click.option('--use-existing', is_flag=True, help='User can input their own secret key.')
+def setup(use_existing):
+
     click.echo('Command working!')
+    if use_existing:
+        click.echo("Option working!")
 
 
-@main.command()
-def get():
+@main.command('get', short_help='get password for service')
+@click.argument('service', metavar='<service>')
+def get(service):
     click.echo('Command working!')
+    click.echo(service)
 
 
-@main.command()
+@main.command('reset', short_help='reset all passwords')
 def reset():
     click.echo('Command working!')
 
